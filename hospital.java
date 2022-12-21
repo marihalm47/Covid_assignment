5a6d70189c4030cd7d3b55afc7ac017a9708df65
Monika morawadkar <marihalm47@gmail.com>
2022-12-20 20:09:32 -0600

    Add files via upload

---
+++ hospital.java
@@ -0,0 +1,88 @@
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class hospital {
+
+	
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "C:\\TestAutomation\\ChromeDriver\\chromedriver_win32\\chromedriver.exe");
+        WebDriver driver =  new ChromeDriver();
+        driver.get("https://westbengal.covidsafe.in/");
+        driver.manage().window().maximize();
+        
+	   int numberOfhosp = driver.findElements(By.xpath("//tbody/tr")).size();
+			
+		for(int i=1; i<numberOfhosp;i++)
+		{
+								
+			//Number of Hospital
+			WebElement hospNameEle = driver.findElement(By.xpath("//tr["+i+"]//strong"));
+			String hospName = hospNameEle.getText();
+			System.out.println("name of the hospital is : "+hospName);
+			
+			//withoutoxygen
+			WebElement WithoutOxygenEle = driver.findElement(By.xpath("//tr["+i+"]/td[2]//span"));
+			String WithoutOxygen = WithoutOxygenEle.getText();
+			System.out.println("Number of Beds Without Oxygen : "+WithoutOxygen);
+			
+			//withoxygen
+			WebElement WithOxygenEle = driver.findElement(By.xpath("//tr["+i+"]/td[3]//span"));
+			String WithOxygen = WithOxygenEle.getText();
+			System.out.println("Number of Beds With Oxygen : "+WithOxygen);
+			
+			
+			//WithoutICU ventilator
+			WebElement ICUwithoutVenEle = driver.findElement(By.xpath("//tr["+i+"]/td[4]//span"));
+			String ICUwithoutVen = ICUwithoutVenEle.getText();
+			System.out.println("Number of Beds Without ICU Ventilator : "+ICUwithoutVen);
+			
+			//With ICU Ventilator
+			WebElement ICUwithVenEle = driver.findElement(By.xpath("//tr["+i+"]/td[5]//span"));
+			String ICUwithVen = ICUwithVenEle.getText();
+			System.out.println("Number of Beds With ICU Ventilator : "+ICUwithVen);
+			
+			int j= i+1;
+			
+			driver.findElement(By.xpath("//tr["+i+"]//strong")).click();  
+			
+			JavascriptExecutor js = (JavascriptExecutor) driver;
+		   // js.executeScript("window.scrollBy(0,-350)", "");
+		  // js.executeScript("arguments[0].scrollIntoView();", Element);
+		       
+		   //driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+    
+			Thread.sleep(1000);
+			WebElement PhoneEle = driver.findElement(By.xpath("//tr["+j+"]/td/p[2]/span"));
+			String Phone = PhoneEle.getText();
+			System.out.println("Hospital "+Phone);
+			
+			WebElement PinCodeEle = driver.findElement(By.xpath("//tr["+j+"]/td/p[3]//span"));
+			String PinCode = PinCodeEle.getText();
+			System.out.println("Hospital " +PinCode);
+			js.executeScript("arguments[0].scrollIntoView();", PinCodeEle);
+			
+			WebElement AddressEle = driver.findElement(By.xpath("//tr["+j+"]/td/p[4]//span"));
+			String Address = AddressEle.getText();
+			System.out.println("Hospital "+Address);
+			
+			//Scroll Down to the adrress element
+			js.executeScript("arguments[0].scrollIntoView();", AddressEle);
+						
+			driver.findElement(By.xpath("//tr["+i+"]//strong")).click();
+			
+			//scroll down to the hospital name element
+			js.executeScript("arguments[0].scrollIntoView();", hospNameEle);
+
+	
+		}
+		
+
+	}
+        
+}
